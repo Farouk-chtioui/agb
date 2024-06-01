11fad1cf355ddc8e55d49b85118999a3e0cb3870
SameOldSteven <chtiouiaa25@gmail.com>
2024-06-01 11:08:57 +0100

    now it works

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './componant/dashboard/dashboard';
 import Login from './componant/login/login';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
       </Routes>
     </Router>
   );
--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ element, ...rest }) => {
+const ProtectedRoute = ({ element }) => {
   const token = localStorage.getItem('token');
 
-  return (
-    <Route
-      {...rest}
-      element={token ? element : <Navigate to="/login" replace />}
-    />
-  );
+  return token ? element : <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute;
