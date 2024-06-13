b5ad686838b13b4527283710b0f3afbbc54009eb
SameOldSteven <chtiouiaa25@gmail.com>
2024-06-13 19:53:43 +0100

    crud for market

--- src/components/magasins/MagasinForm.js
+++ src/components/magasins/MagasinForm.js
@@ -11,10 +11,12 @@ const MagasinForm = ({
 })=>{
     const fields = [
         { name: 'first_name', label: 'Nom', type: 'text', placeholder: 'Nom', colSpan: 1 },
-        {name:'last_name',label:'Last Name',type:'text',placeholder:'Last Name',colSpan:1},
+        {name:'last_name',label:'Prenom',type:'text',placeholder:'Prenom',colSpan:1},
         { name: 'email', label: 'Email', type: 'email', placeholder: 'Email', colSpan: 1 },
         {name:'password' ,label:'Password',type:'password',placeholder:'Password',colSpan:1},
-        { name: 'description', label: 'Description', type: 'text', placeholder: 'Description', colSpan: 2 },
+        {name:'address',label:'Address',type:'text',placeholder:'Address',colSpan:1},
+        {name:'numberMa',label:'Nombre des commandes du matin',type:'number',placeholder:'Nombre des commandes du matin',colSpan:1},
+        {name:'numberMi',label:'Nombre des commandes du midi',type:'number',placeholder:'Nombre des commandes du midi',colSpan:1},
     ].filter(Boolean);
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -35,3 +37,4 @@ const MagasinForm = ({
         />
     );
 }
+export default MagasinForm;
--- src/components/magasins/Magasins.js
+++ src/components/magasins/Magasins.js
@@ -1,9 +1,155 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react';
+import { fetchMagasins, deleteMagasin, addMagasin, searchMagasins, modifyMagasin } from '../../api/Auth';
+import Dashboard from '../dashboard/Dashboard';
+import Search from '../searchbar/Search';
+import MagasinForm from './MagasinForm';
+import MagasinTable from './MagasinTable';
+import Pagination from '../Pagination/Pagination';
 
-function magasins() {
+const Magasins = () => {
+  const [magasins, setMagasins] = useState([]);
+  const [filteredMagasins, setFilteredMagasins] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [showForm, setShowForm] = useState(false);
+  const [newMagasin, setNewMagasin] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    address: '',
+    numberMa: '',
+    numberMi: ''
+  });
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [currentMagasin, setCurrentMagasin] = useState(null);
+  const [isSearchActive, setIsSearchActive] = useState(false);
+
+  useEffect(() => {
+    fetchMagasinsData();
+  }, [currentPage]);
+
+  const fetchMagasinsData = async () => {
+    try {
+      const data = await fetchMagasins(currentPage);
+      setMagasins(data);
+      setFilteredMagasins(data);
+    } catch (error) {
+      console.error('Error fetching magasins', error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await deleteMagasin(id);
+      fetchMagasinsData();
+    } catch (error) {
+      console.error('Error deleting magasin', error);
+    }
+  };
+
+  const handleModify = (magasin) => {
+    setCurrentMagasin(magasin);
+    setNewMagasin(magasin);
+    setIsEditMode(true);
+    setShowForm(true);
+  };
+
+  const handleEditMagasin = async (e) => {
+    e.preventDefault();
+    try {
+      await modifyMagasin(newMagasin);
+      fetchMagasinsData();
+      setShowForm(false);
+      setIsEditMode(false);
+      setCurrentMagasin(null);
+      setNewMagasin({
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        address: '',
+        numberMa: '',
+        numberMi: ''
+      });
+    } catch (error) {
+      console.error('Error modifying magasin', error);
+    }
+  };
+
+  const handleAddMagasin = async (e) => {
+    e.preventDefault();
+    try {
+      await addMagasin(newMagasin);
+      fetchMagasinsData();
+      setShowForm(false);
+    } catch (error) {
+      console.error('Error adding magasin', error);
+    }
+  };
+
+  const handleChange = useCallback((e) => {
+    const { name, value,type} = e.target;
+    setNewMagasin(prevState => ({
+      ...prevState,
+      [name]: type === 'number' ? Number(value) : value,
+    }));
+  }, []);
+
+  const handleSearch = async (searchTerm) => {
+    if (searchTerm === '') {
+      setFilteredMagasins(magasins);
+      setIsSearchActive(false);
+    } else {
+      try {
+        const response = await searchMagasins(searchTerm);
+        setFilteredMagasins(response);
+        setIsSearchActive(true);
+      } catch (error) {
+        console.error('Error searching magasins', error);
+      }
+    }
+  };
+
   return (
-    <div>magasins</div>
-  )
-}
+    <div className="flex">
+      <Dashboard title="Gérer les magasins" />
+      <div className="flex-1 container mx-auto p-9 relative mt-20 ">
+        <Search setData={handleSearch} title={"Tous les magasins"} />
+        <button
+          className="custom-color2 text-white px-4 py-2 rounded mb-4 absolute top-0 right-0 mt-4 mr-4 shadow hover:bg-blue-600 transition"
+          onClick={() => {
+            setShowForm(true);
+            setIsEditMode(false);
+            setNewMagasin({
+              first_name: '',
+              last_name: '',
+              email: '',
+              password: '',
+              address: '',
+              numberMa: '',
+              numberMi: ''
+            });
+          }}>
+          Ajouter un magasin
+        </button>
 
-export default magasins
+        {showForm && (
+          <MagasinForm
+            newMagasin={newMagasin}
+            handleChange={handleChange}
+            handleAddMagasin={handleAddMagasin}
+            handleEditMagasin={handleEditMagasin}
+            setShowForm={setShowForm}
+            isEditMode={isEditMode}
+          />
+        )}
+
+        <MagasinTable magasins={filteredMagasins} handleDelete={handleDelete} handleModify={handleModify} />
+
+        {!isSearchActive && <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} />}
+      </div>
+    </div>
+  );
+};
+
+export default Magasins;
