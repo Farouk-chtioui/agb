d8a7fe3b219c4838aa9d81f9e0dd26203f4acfd6
SameOldSteven <chtiouiaa25@gmail.com>
2024-06-30 03:59:34 +0100

    refactor: Update PublicRoute to redirect based on user role

--- src/publicroute.js
+++ src/publicroute.js
@@ -1,11 +1,28 @@
-// PublicRoute.js
 import { Navigate, useLocation } from 'react-router-dom';
 
 const PublicRoute = ({ element }) => {
   const token = localStorage.getItem('token');
+  const role = localStorage.getItem('role');
   const location = useLocation();
 
-  return token && location.pathname === '/' ? <Navigate to="/dashboard" replace /> : element;
+  const getRedirectPath = (role) => {
+    switch (role) {
+      case 'admin':
+        return '/admin/dashboard';
+      case 'market':
+        return '/market/dashboard';
+      case 'driver':
+        return '/driver/dashboard';
+      case 'user':
+        return '/user/dashboard';
+      default:
+        return '/';
+    }
+  };
+
+  const redirectPath = getRedirectPath(role);
+
+  return token && location.pathname === '/' ? <Navigate to={redirectPath} replace /> : element;
 };
 
 export default PublicRoute;
