b16954cf5e09e797b11aadf0d3c552e11984be4c
SameOldSteven <chtiouiaa25@gmail.com>
2024-07-04 11:58:18 +0100

    refactor: Update SectureTable component to improve code readability and add filtering for postal codes

--- src/components/Plans/Secteurs/SecteursTable.js
+++ src/components/Plans/Secteurs/SecteursTable.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import Table from '../../Table/Table';
-
+import './SectureTable.css';
 const SectureTable = ({ sectures, handleDelete, handleModify }) => {
     const header = ["Nom de Secture", "Codes Postaux", "Action"];
     const role = localStorage.getItem('role');
 
     const renderRow = (secture) => (
         <>
-            <td className="py-2 px-4 border-b border-gray-200">{secture.name}</td>
-            <td className="py-2 px-4 border-b border-gray-200">
-                <ul className="list-none p-0 m-0">
-                    {secture.codesPostaux.map((code, index) => (
-                        <li key={index} className="relative pl-4 before:content-['•'] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:text-black">
-                            {code}
-                        </li>
-                    ))}
+            <td className="py-2 px-4 border-b border-gray-200 centered-cell">{secture.name}</td>
+            <td className="py-2 px-4 border-b border-gray-200 centered-cell">
+                <ul className="list-none p-0 m-0 postal-code-list">
+                    {secture.codesPostaux
+                        .filter(code => code !== 0)
+                        .map((code, index) => (
+                            <li key={index} className="postal-code-item">
+                                <span className="bullet">•</span>
+                                <span className="fixed-width">{code}</span>
+                            </li>
+                        ))}
                 </ul>
             </td>
         </>
     );
+
     return (
-        <Table headers={header} data={sectures} renderRow={renderRow} handleDelete={handleDelete} handleModify={handleModify} role={role}/>
+        <Table 
+            headers={header} 
+            data={sectures} 
+            renderRow={renderRow} 
+            handleDelete={handleDelete} 
+            handleModify={handleModify} 
+            role={role} 
+        />
     );
 }
 
