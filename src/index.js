4addf19bd3ceb69264baacd59a9c7d23c0002c86
SameOldSteven <chtiouiaa25@gmail.com>
2024-07-11 13:49:36 +0100

    refactor: Remove unused Google Maps script from index.html and dynamically load it in index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,13 @@ root.render(
     <App />
     </Router>
 );
-
+document.addEventListener('DOMContentLoaded', function() {
+    var script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+  });
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
